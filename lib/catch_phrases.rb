031ab38f39e60f44a1635a2965e1792658b72846
Allen Palacios <apalacios1994@gmail.com>
2021-02-09 03:08:54 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,21 +4,16 @@ def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
-
 def toadstool
-  puts $status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
 end
-toadstool
-
 def link
-  c_phrase = "It's Dangerous To Go Alone! Take This."
-  puts c_phrase
+  link = "It's Dangerous To Go Alone! Take This."
+  puts link
 end
-link
-
 def all_phrases
   puts mario
   puts toadstool
   puts link
 end
-all_phrases
