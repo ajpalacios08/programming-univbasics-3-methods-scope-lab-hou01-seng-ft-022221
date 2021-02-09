20617fcf08870ebdb5395cff81d0934c3b3be5df
Allen Palacios <apalacios1994@gmail.com>
2021-02-09 02:54:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,18 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
+mario
 
 def toadstool
-  puts status
+  puts $status
 end
+toadstool
+
+def link
+  c_phrase = "It's Dangerous To Go Alone! Take This."
+  puts c_phrase
+end
+link
--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+    puts phrase
   end
 end
 
