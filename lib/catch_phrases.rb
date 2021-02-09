b1208233dcc20f4d08701fec160334ac000d2323
Allen Palacios <apalacios1994@gmail.com>
2021-02-09 02:56:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,10 @@
 $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-$phrase = "It's-a me, Mario!"
+
 
 def mario
-  puts $phrase
+  phrase = "It's-a me, Mario!"
+  puts phrase
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 mario
 
